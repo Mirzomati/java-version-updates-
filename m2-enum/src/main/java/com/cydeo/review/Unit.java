343587ea28149c1701c836dd8028a48c476023ef
Muzaffarzoda <muzaffarzoda@gmail.com>
2024-08-21 14:12:38 -0400

    enum review

---
+++ m2-enum/src/main/java/com/cydeo/review/Unit.java
@@ -0,0 +1,15 @@
+package com.cydeo.review;
+
+public enum Unit {
+
+    PCS("pieces"),
+    POUNDS("pounds"),
+    INCHES("inches"),
+    MILES("miles");
+
+    public String value;
+
+    Unit(String value) {
+        this.value = value;
+    }
+}
